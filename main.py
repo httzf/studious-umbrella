2496d96de9f607fd5580a0a247c1e1fc0f6c932d
mary <148317021+httzf@users.noreply.github.com>
2024-01-19 16:14:31 +0300

    все ссылки работают

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return f"""
+@app.route('/') # endpoint, по умолчанию заходит под /
+def hello():    # функция представления
+     return f"""
     <a href={url_for('index')}>Site</a>
 """
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') # отрисовывает шаблон
 
+@app.route('/day-<num>')
+def day(num):
+    return render_template(f'day-{num}.html') # отрисовывает шаблон
 
-
+@app.route('/photo-<num>')
+def photo(num):
+    return render_template(f'photo-{num}.html') # отрисовывает шаблон
 
 
 if __name__ == '__main__':
